696998e04be11ec88a2996d2ae15fa910da854bc
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2019-10-03 00:18:22 -0700

    Log can now prepopulate tags

--- test/log_test.exs
+++ test/log_test.exs
@@ -1,14 +1,27 @@
 defmodule LogTest do
   use ExUnit.Case
-  require Logger
 
   defmodule Deeply.Nested.Module.WithLog do
-    def log do
-      Logger.error("foo", tags: [:foo])
+    use Log.Macros, tags: [:use]
+
+    @impl true
+    def bare_log(chars_or_fun, meta) do
+      IO.inspect({chars_or_fun, meta})
+    end
+
+    @log_tags [:other_tag]
+    def hello do
+      log(:error, "foo", tags: [:foo])
+    end
+
+    @log_tags [:special_other]
+    def world do
+      log(:error, "bar", tags: [:bar])
     end
   end
 
   test "foo" do
-    Deeply.Nested.Module.WithLog.log()
+    Deeply.Nested.Module.WithLog.hello()
+    Deeply.Nested.Module.WithLog.world()
   end
 end
